1033641fec251b3a7df0c278451b3f9f796e45a1
YmkTry <106361485+ymk11@users.noreply.github.com>
2024-04-19 16:17:12 -0400

    Update piece.hpp

--- TP6/piece.hpp
+++ TP6/piece.hpp
@@ -18,8 +18,6 @@ public:
     virtual const TypePiece& getTypePiece() const;
     virtual const Couleur& getCouleur() const ;
     virtual  const QString& getImage() const;
-    //J'ai du enlever echequier en argument à cause de définition de class circulaire. ça causait des erreurs de  compilation. à la place,
-    //c'est l'échéquier qui checkera si les déplacements sont valides ou non
     virtual std::vector<Position> getListeDeplacements(const Position& depart, const Echiquier&) const = 0;
 
 private:
